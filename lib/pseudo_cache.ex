f5f3920cc23a9b53574b7fdf121ee4c31b1e740f
Andrzej Niewiadomski <andrzej@telnyx.com>
2020-02-29 11:21:39 +0100

    Add logic for put, get and delete

--- lib/pseudo_cache.ex
+++ lib/pseudo_cache.ex
@@ -3,16 +3,43 @@ defmodule PseudoCache do
   Documentation for PseudoCache.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  def start_link(cache \\ %{}) when is_map(cache) do
+    GenServer.start_link(__MODULE__, cache)
+  end
 
-      iex> PseudoCache.hello()
-      :world
+  def put(pid, key, element) do
+    GenServer.cast(pid, {:put, key, element})
+  end
 
-  """
-  def hello do
-    :world
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def delete(pid, key) do
+    GenServer.cast(pid, {:delete, key})
+  end
+
+  # Server (callbacks)
+
+  @impl true
+  def init(cache) do
+    {:ok, cache}
+  end
+
+  @impl true
+  def handle_cast({:put, key, element}, state) do
+    {:noreply, Map.put(state, key, element)}
+  end
+
+  @impl true
+  def handle_call({:get, key}, _from, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  @impl true
+  def handle_cast({:delete, key}, state) do
+    {:noreply, Map.delete(state, key)}
   end
 end
